97f6ffd3bff52e723148c73b7b870559b16a7ac2
Jayapriya7598 <148631441+Jayapriya7598@users.noreply.github.com>
2023-10-21 18:44:03 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,7 @@
+n=int(input("enter the number :"))
+Def factorial (n) :
+If n==0:
+return  1
+else:
+return n* factorial (n-1)
+Print(factorial(n))
---
+++ main (2).py
@@ -0,0 +1,5 @@
+Y=int(input("Enter the year:"))
+if(y%4==0):
+    print("leap year")
+else:
+    print("Not a leap year")
